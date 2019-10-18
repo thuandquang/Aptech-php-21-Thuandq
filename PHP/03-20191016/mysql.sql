8744d92457740b468307bb46926a6e2d7bbb5d47
thuandquang <thuan.qdang.mcs@gmail.com>
2019-10-18 18:20:59 +0700

    .

--- PHP/03-20191016/mysql.sql
+++ PHP/03-20191016/mysql.sql
@@ -1,22 +1,64 @@
-CREATE DATABASE AP_21_3;
-
-CREATE TABLE AP_21_3.users(
-	id int,
-    last_name varchar(50),
-    first_name varchar(50),
-    address varchar(50),
-    city varchar(100)
+CREATE DATABASE APTECH;
+DROP DATABASE APTECH;
+-- tao table users
+CREATE TABLE APTECH.users(
+    id int,
+    last_name varchar(255),
+    first_name varchar(255),
+    email varchar(255) unique,
+    created_at datetime,
+    modified_at datetime,
+    deleted_at datetime
 );
 
-INSERT INTO AP_21_3.users(id,last_name,first_name)
-VALUES(1,'DANG','THUAN');
-INSERT INTO AP_21_3.users(id,last_name,first_name)
-VALUES(2,'DANG','A');
-INSERT INTO AP_21_3.users(id,last_name,first_name)
-VALUES(3,'DANG','B');
-INSERT INTO AP_21_3.users(id,last_name,first_name)
-VALUES(4,'DANG','C');
-INSERT INTO AP_21_3.users(id,last_name,first_name)
-VALUES(5,'DANG','D');
+-- xoa table users
+DROP TABLE APTECH.users;
 
+-- chinh sua table 
+	-- them cot ngay sinh
+ALTER TABLE APTECH.users 
+	ADD dob date; 
+    -- xoa cot ngay sinh
+ALTER TABLE APTECH.users 
+	DROP COLUMN DOB; 
+    
+-- nhap du lieu cho table
+INSERT INTO APTECH.users
+    (id,last_name, first_name, email, created_at)
+VALUES
+    (1, 'Nam', 'Nguyen', 'namnh.website@gmail.com', NOW()),
+    (2, 'John', 'Cenna', 'cenna.john@hotmail.com', NOW()),
+    (3, 'Henry', 'Tran', 'tranhe@resolutioninc.com', NOW()),
+    (4, 'Christiaan', 'Hunter', 'ch.pageworth@pageworth.com', NOW()),
+    (5, 'Vicky', 'Nguyen', 'vicky06@gmail.com', NOW());
+    
+-- hien thi tat ca thong tin trong users 
+SELECT * FROM APTECH.users;
+-- hien thi ten
+SELECT DISTINCT last_name
+FROM APTECH.users;
+-- hien thi top 3 dau tien
+SELECT * FROM APTECH.users
+	LIMIT 3; 
+-- hien thi nguoi ten Nam
+SELECT * FROM APTECH.users
+	WHERE last_name = 'Nam';
+ 
+-- tim ho Tran doi ten thanh Dan
+UPDATE APTECH.users 
+	SET last_name = 'Dan'
+	WHERE first_name = 'Tran';
+-- xoa nguoi co id la 5
+DELETE FROM APTECH.users
+WHERE id = 5;
 
+-- dat id la khoa chinh(Primary key) va set cho tu dong tang dan
+ALTER TABLE APTECH.users
+	ADD PRIMARY KEY(id);
+ALTER TABLE APTECH.users
+	MODIFY COLUMN id int NOT NULL AUTO_INCREMENT ;
+    
+INSERT INTO APTECH.users
+    (last_name, first_name, email, created_at)
+VALUES
+    ('Stark', 'Tony', 'ironman_stack@gmail.com', NOW());
